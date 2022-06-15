826f0e78d2b69811beee991d4e83595812aaaff4
SDDNG <davidangraham@gmail.com>
2022-06-15 15:13:42 +0000

    fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['daveg-django-demo-todo-app.herokuapp.com/']
+ALLOWED_HOSTS = ['daveg-django-demo-todo-app.herokuapp.com']
 
 
 # Application definition
